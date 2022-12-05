e2d0cc220a7d520cb35547a72aafe132f4ab7f41
Oliver Heywood <oliver.heywood@sitecore.com>
2022-12-04 20:13:52 -0600

    couple route changes need so swagger spec builds

--- Customer.OrderCloud.Api/Controllers/MessageSenderController.cs
+++ Customer.OrderCloud.Api/Controllers/MessageSenderController.cs
@@ -30,42 +30,41 @@ namespace Customer.OrderCloud.Api.Controllers
 			Route("NewUserInvitation"), 
 			Route("ForgottenPassword")]
 		[OrderCloudWebhookAuth] // Security feature to verifiy request came from Ordercloud.
-		public async Task NewUserInvitation(SetPasswordMessageSenderPayloadWithXp payload)
+		public async Task NewUserInvitation([FromBody] SetPasswordMessageSenderPayloadWithXp payload)
 			=> await _command.SendSetPasswordEmail(payload);
 
 
-		// These message sender types share payloads and default template data fields. They should have unqiue templates. They can be split into separate actions as needed. 
-		[HttpPost, 
-			Route("OrderSubmitted"),
-			Route("OrderSubmittedForApproval"),
-			Route("OrderApproved"),
-			Route("OrderDeclined"),
-			Route("OrderSubmittedForYourApproval"),
-			Route("OrderSubmittedForYourApprovalHasBeenApproved"),
-			Route("OrderSubmittedForYourApprovalHasBeenDeclined")]
-		[OrderCloudWebhookAuth] // Security feature to verifiy request came from Ordercloud.
-		public async Task OrderSubmitted(OrderMessageSenderPayloadWithXp payload)
+	   //These message sender types share payloads and default template data fields.They should have unqiue templates.They can be split into separate actions as needed.
+	   [HttpPost,
+		   Route("OrderSubmitted"),
+		   Route("OrderSubmittedForApproval"),
+		   Route("OrderApproved"),
+		   Route("OrderDeclined"),
+		   Route("OrderSubmittedForYourApproval"),
+		   Route("OrderSubmittedForYourApprovalHasBeenApproved"),
+		   Route("OrderSubmittedForYourApprovalHasBeenDeclined")]
+	   [OrderCloudWebhookAuth] // Security feature to verifiy request came from Ordercloud.
+		public async Task OrderSubmitted([FromBody] OrderMessageSenderPayloadWithXp payload)
 			=> await _command.SendOrderEmail(payload);
 
 
 		[HttpPost, Route("ShipmentCreated")]
 		[OrderCloudWebhookAuth] // Security feature to verifiy request came from Ordercloud.
-		public async Task ShipmentCreated(ShipmentCreatedMessageSenderPayloadWithXp payload)
+		public async Task ShipmentCreated([FromBody] ShipmentCreatedMessageSenderPayloadWithXp payload)
 			=> await _command.SendShipmentCreatedEmail(payload);
 
-
-		// These message sender types share payloads and default template data fields. They should have unqiue templates. They can be split into separate actions as needed. 
-		[HttpPost, 
-			Route("OrderReturnSubmitted"),
-			Route("OrderReturnSubmittedForApproval"),
-			Route("OrderReturnApproved"),
-			Route("OrderReturnDeclined"),
-			Route("OrderReturnSubmittedForYourApproval"),
-			Route("OrderReturnSubmittedForYourApprovalHasBeenDeclined"),
-			Route("OrderReturnSubmittedForYourApprovalHasBeenDeclined"),
-			Route("OrderReturnCompleted")]
-		[OrderCloudWebhookAuth] // Security feature to verifiy request came from Ordercloud.
-		public async Task OrderReturnSubmitted(OrderReturnMessageSenderPayloadWithXp payload)
+	   //These message sender types share payloads and default template data fields.They should have unqiue templates.They can be split into separate actions as needed.
+	   [HttpPost,
+		   Route("OrderReturnSubmitted"),
+		   Route("OrderReturnSubmittedForApproval"),
+		   Route("OrderReturnApproved"),
+		   Route("OrderReturnDeclined"),
+		   Route("OrderReturnSubmittedForYourApproval"),
+		   Route("OrderReturnSubmittedForYourApprovalHasBeenApproved"),
+		   Route("OrderReturnSubmittedForYourApprovalHasBeenDeclined"),
+		   Route("OrderReturnCompleted")]
+	   [OrderCloudWebhookAuth] // Security feature to verifiy request came from Ordercloud.
+		public async Task OrderReturnSubmitted([FromBody] OrderReturnMessageSenderPayloadWithXp payload)
 			=> await _command.SendOrderReturnEmail(payload);
 	}
 }
